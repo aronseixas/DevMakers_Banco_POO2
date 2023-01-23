a68661af59380e620999c493c1c1ee66e0a9477f
Luccas <lccs.mrm@gmail.com>
2023-01-23 14:07:50 -0300

    Atualização do MenuOperacoes, chamando os métodos dos controllers. Este menu está sendo chamado dentro da classe "Logar", que está nos controllers, após a validação das credenciais do cliente.

--- src/br/com/ada/poo2/banco/controllers/operacoes/Investir.java
+++ src/br/com/ada/poo2/banco/controllers/operacoes/Investir.java
@@ -1,5 +1,7 @@
 package br.com.ada.poo2.banco.controllers.operacoes;
 
 public class Investir {
+    public static void executarInvestimento() {
+    }
     //Deve retornar MenuOperacoes ao final
 }
--- src/br/com/ada/poo2/banco/controllers/operacoes/Transferir.java
+++ src/br/com/ada/poo2/banco/controllers/operacoes/Transferir.java
@@ -1,5 +1,7 @@
 package br.com.ada.poo2.banco.controllers.operacoes;
 
 public class Transferir {
+    public static void executarTransferencia() {
+    }
     //Deve retornar MenuOperacoes ao final
 }
